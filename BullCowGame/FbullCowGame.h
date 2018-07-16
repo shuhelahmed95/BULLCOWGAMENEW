cd9a911af9eae6c16899b9d7afd6485d987f5b1d
shuhelahmed95 <32647616+shuhelahmed95@users.noreply.github.com>
2018-07-16 18:31:49 +0100

    codeblocks update

--- BullCowGame/FbullCowGame.h
+++ BullCowGame/FbullCowGame.h
@@ -1,37 +1,35 @@
-#pragma once
+
+ #pragma oncen
 #include <string>
 
 using FString = std::string;
 using int32 = int;
 
-struct BullCowCount
-	{
+// all values intialised to zero
+struct FBullCowCount
+{
 	int32 Bulls = 0;
 	int32 Cows = 0;
-	};
+};
 
-
-class FbullCowGame	
-	{
-
+class FBullCowGame
+{
 public:
-	FbullCowGame();  //CONSTRUCTOR 
+	FBullCowGame(); // constructor
+
 	int32 GetMaxTries() const;
 	int32 GetCurrentTry() const;
 	bool IsGameWon() const;
 
 	void Reset(); // TODO make a more rich return value.
-	bool CheckGuessValidity(FString);// TODO make a more rich return value.
-
-	
-	BullCowCount SubmitGuess(FString);
-	
+	bool CheckGuessValidity(FString); // TODO make a more rich return value.
+	FBullCowCount SubmitGuess(FString Guess);
 
 
-
+// ^^ Please try and ignore this and focus on the interface above ^^
 private:
-	int32 MyCurrentTry; //see constructor for initalisation.
-	int32 MyMaxTries;
-	FString  MyHiddenWord;
-
-	};
+	// see constructor for initialisation
+	int32 MyCurrentTry;           // I am the game's current try.
+	int32 MyMaxTries;             // I am the game's maximum try.
+	FString MyHiddenWord;         // I am the game's hidden word
+};
